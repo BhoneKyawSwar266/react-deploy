7817b6babe1d90e69d5891d6d0d2ef6cfc664450
MinnMinn <adramblink@gmail.com>
2024-04-23 00:04:06 +0630

    Changes

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import '../Item/Item.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../Item/Item.css";
+import { Link } from "react-router-dom";
 
 const Item = (props) => {
   return (
-    <div className='item'>
-       <Link to={`/product/${props.id}`}> <img src={props.image} alt="" onClick={window.scrollTo(0,0)}/></Link> 
+    <Link to={`/product/${props.id}`}>
+      <div className="item">
+        <img src={props.image} alt="" onClick={window.scrollTo(0, 0)} />
         <p>{props.name}</p>
         <div className="item-prices">
-            <div className="item-price-new">
-                {props.new_price} MMK
-            </div>
-            {props.old_price ? (
-            <div className="item-price-old">
-              {props.old_price} MMK
-            </div>
-    ) : null}
+          <div className="item-price-new">{props.new_price} MMK</div>
+          {props.old_price ? (
+            <div className="item-price-old">{props.old_price} MMK</div>
+          ) : null}
         </div>
-    </div>
-  )
-}
+      </div>
+    </Link>
+  );
+};
 
-export default Item
+export default Item;
